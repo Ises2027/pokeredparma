3468d5344ad44457553bf7445ef4ddbeac761cb4
Isès <106678650+Ises2027@users.noreply.github.com>
2025-01-26 04:40:46 +0100

    Updates casino prizes

--- data/events/prizes.asm
+++ data/events/prizes.asm
@@ -9,49 +9,25 @@ NoThanksText:
 PrizeMenuMon1Entries:
 	db ABRA
 	db CLEFAIRY
-IF DEF(_RED)
-	db NIDORINA
-ENDC
-IF DEF(_BLUE)
-	db NIDORINO
-ENDC
+	db DRATINI
 	db "@"
 
 PrizeMenuMon1Cost:
-IF DEF(_RED)
-	bcd2 180
-	bcd2 500
-ENDC
-IF DEF(_BLUE)
-	bcd2 120
-	bcd2 750
-ENDC
-	bcd2 1200
+	bcd2 160
+	bcd2 625
+	bcd2 3650
 	db "@"
 
 PrizeMenuMon2Entries:
-IF DEF(_RED)
-	db DRATINI
-	db SCYTHER
-ENDC
-IF DEF(_BLUE)
 	db PINSIR
-	db DRATINI
-ENDC
+	db SCYTHER
 	db PORYGON
 	db "@"
 
 PrizeMenuMon2Cost:
-IF DEF(_RED)
-	bcd2 2800
-	bcd2 5500
-	bcd2 9999
-ENDC
-IF DEF(_BLUE)
-	bcd2 2500
-	bcd2 4600
-	bcd2 6500
-ENDC
+	bcd2 5000
+	bcd2 5000
+	bcd2 8200
 	db "@"
 
 PrizeMenuTMsEntries:
